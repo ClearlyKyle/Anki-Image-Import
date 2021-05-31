ab29829109981bb04a5309b3f798de697ce25979
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-05-31 23:22:18 +0100

    added progress bar

--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
-import base64
 from aqt import mw
 from aqt.utils import showInfo, qconnect
 from aqt.qt import *
+import time
 
 
 class AnkiImageImport(QDialog):
-    NumGridRows = 3
-    NumButtons = 4
+    # _NumGridRows = 3
+    # _NumButtons = 4
+
+    # setting  the fixed width of window
+    _width = 300
+    _height = 200
+
+    _signal = pyqtSignal(int)
 
     def __init__(self):
         QDialog.__init__(self, parent=mw)
@@ -14,8 +20,7 @@ class AnkiImageImport(QDialog):
 
         self.CreateFormGroupBox()
 
-        buttonBox = QDialogButtonBox(
-            QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttonBox.accepted.connect(self.StartButtonFunction)
         buttonBox.rejected.connect(self.OnReject)
 
@@ -23,11 +28,8 @@ class AnkiImageImport(QDialog):
         mainLayout.addWidget(self.formGroupBox)
         mainLayout.addWidget(buttonBox)
 
-        # setting  the fixed width of window
-        width = 300
-        height = 200
-        self.setFixedWidth(width)
-        self.setFixedHeight(height)
+        self.setFixedWidth(self._width)
+        self.setFixedHeight(self._height)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("Anki Picture Importer - Settings")
@@ -71,8 +73,8 @@ class AnkiImageImport(QDialog):
         hbox = QHBoxLayout()
         self.subdirectory_checkbox = QCheckBox("Search Subdirectories")
         self.subdirectory_checkbox.setChecked(False)
-        # r2 = QCheckBox("Delete")
         hbox.addWidget(self.subdirectory_checkbox, 0, Qt.AlignRight)
+        # r2 = QCheckBox("Delete")
         # hbox.addWidget(r2, 0)
         layout.addRow(hbox)
 
@@ -82,21 +84,23 @@ class AnkiImageImport(QDialog):
         self.subdirectory_checkbox.setEnabled(False)
 
         # Get path to Image Folder
-        self.folderpath = QFileDialog.getExistingDirectory(self, 'Select Folder')
-        #showInfo("folderpath\n{}".format(self.folderpath))
+        self.folderpath = QFileDialog.getExistingDirectory(self, "Select Folder")
+        # showInfo("folderpath\n{}".format(self.folderpath))
 
-        if self.folderpath == "": return    # No path selected
-        
+        if self.folderpath == "":
+            return  # No path selected
+
         self.file_path_box.setText(self.folderpath)
 
         self.image_paths = []
 
         for path, subdirs, files in os.walk(self.folderpath):
             for image_name in files:
-                if image_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+                if image_name.lower().endswith((".png", ".jpg", ".jpeg")):
                     self.image_paths.append(os.path.join(path, image_name))
 
-            if self.subdirectory_checkbox.isChecked() is False: break
+            if self.subdirectory_checkbox.isChecked() is False:
+                break
 
         self.formGroupBox.setTitle("{} images found".format(len(self.image_paths)))
         self.subdirectory_checkbox.setEnabled(True)
@@ -111,8 +115,8 @@ class AnkiImageImport(QDialog):
         self.fields_comboBox.addItems(fields)
 
     def StartButtonFunction(self):
-        if self.duplicate_checkbox.isChecked():
-            showInfo("{}".format("Check Box Is True"))
+        # if self.duplicate_checkbox.isChecked():
+        #    showInfo("{}".format("Check Box Is True"))
 
         selected_deck = self.deck_comboBox.currentText()
         selected_model = self.model_comboBox.currentText()
@@ -120,39 +124,85 @@ class AnkiImageImport(QDialog):
 
         # Set the model
         set_model = mw.col.models.byName(selected_model)
-        mw.col.decks.current()['mid'] = set_model['id']
-        showInfo("MODEL\n{}".format(set_model))
+        mw.col.decks.current()["mid"] = set_model["id"]
+        # showInfo("MODEL\n{}".format(set_model))
 
         # Get the deck
         deck = mw.col.decks.byName(selected_deck)
-        showInfo("DECK\n{}".format(deck))
+        # showInfo("DECK\n{}".format(deck))
 
         # Fields
         fields = mw.col.models.fieldMap(set_model)
         field_index = fields[selected_field][0]
 
-        for image in self.image_paths:
-            # image_path = os.path.join(path, image_name)
+        # Open Progress Bar window
+        progressbar = ProgressWindow()
+        progressbar.show()
 
+        self.hide()  # hide options window
+
+        progressbar_steps = 100 / len(self.image_paths)
+
+        for idx, image_path in enumerate(self.image_paths):
             # Write image to collection.media folder and return its new Filename
             new_filename = mw.col.media.add_file(image_path)
 
-            # Create Cards in selected deck with Images selected
-            showInfo("{}".format(new_filename))
-
+            # Create a new Note to add image to
             new_note = mw.col.newNote()
-            new_note.model()['did'] = deck['id']
+            new_note.model()["did"] = deck["id"]
 
             image_field = '<img src="' + new_filename + '" />'
             new_note.fields[field_index] = image_field
 
             mw.col.addNote(new_note)
 
+            # self._signal.emit(idx)
+            progressbar.setValue(idx * progressbar_steps)
+            time.sleep(1)
+            QApplication.processEvents()
+
         mw.col.save()
+        progressbar.close()
+        self.show()
 
     def OnReject(self):
-        # QDialog.reject(self)
         self.close()
+
+
+class ProgressWindow(QDialog):
+    def __init__(self):
+        QDialog.__init__(self, parent=mw)
+
+        self.setWindowTitle("Progress")
+
+        self.pbar = QProgressBar(self)
+        self.pbar.setValue(0)
+
+        self.resize(300, 100)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.pbar)
+        self.setLayout(self.vbox)
+
+        self.show()
+
+    def setValue(self, val):
+        self.pbar.setValue(val)
+
+
+# class Thread(QThread):
+#    progress_update = QtCore.Signal(int) # or pyqtSignal(int)
+
+#    def __init__(self):
+#        QThread.__init__(self)
+
+#    def __del__(self):
+#        self.wait()
+
+#    def run(self):
+#        for i in range(100):
+#            time.sleep(0.1)
+#            self._signal.emit(i)
 
 
 def StartApplication() -> None:
